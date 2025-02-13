edcef9965dbe7be21f90c75e82e0fa20168cc228
Michel Zarpelon <michelzarpe@gmail.com>
2025-02-13 20:08:24 -0300

    crud completo com testes

--- StoreCustomer/contracts/StoreCustomers.sol
+++ StoreCustomer/contracts/StoreCustomers.sol
@@ -7,11 +7,14 @@ contract StoreCustomers {
         uint8 age;
     }
 
+    address private immutable owner;
     mapping(uint32 => Customer) public customers;
-
     uint32 public nextId = 0;
-
     uint32 public count = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function getNextId() private returns (uint32) {
         return ++nextId;
@@ -29,5 +32,37 @@ contract StoreCustomers {
 
     function getCount() public view returns (uint32) {
         return count;
+    }
+
+    function compareStrings(
+        string memory a,
+        string memory b
+    ) private pure returns (bool) {
+        return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
+
+    function editCustomer(uint32 id, Customer memory newCustomer) public {
+        Customer memory oldCustomer = customers[id];
+
+        if (bytes(oldCustomer.name).length == 0) return;
+
+        if (
+            bytes(newCustomer.name).length > 0 &&
+            !compareStrings(oldCustomer.name, newCustomer.name)
+        ) oldCustomer.name = newCustomer.name;
+
+        if (newCustomer.age > 0 && oldCustomer.age != newCustomer.age)
+            oldCustomer.age = newCustomer.age;
+
+        customers[id] = oldCustomer;
+    }
+
+    function removeCustomer(uint32 id) public {
+        require(msg.sender == owner, "Caller is not owner");
+        Customer memory oldCustomer = customers[id];
+        if (bytes(oldCustomer.name).length > 0) {
+            delete customers[id];
+            count--;
+        }
     }
 }
--- StoreCustomer/test/StoreCustomer.test.js
+++ StoreCustomer/test/StoreCustomer.test.js
@@ -49,4 +49,29 @@ contract('StoreCustomers', function (accounts) {
         assert.equal(customer.name, "Zé", "The customer name should be Zé");
     });
 
+    it('Edit Customer', async () => {
+        await contract.addCustomer({
+            name: "Luiz",
+            age: 34
+        });
+     
+        await contract.editCustomer(1, { name: "Fernando", age: 0 });
+     
+        const customer = await contract.getCustomer(1);
+        assert(customer.name === "Fernando" && customer.age === 34, "Couldn't edit the customer.");
+    })
+
+    it('Remove Customer', async () => {
+        await contract.addCustomer({
+            name: "Luiz",
+            age: 34
+        });
+     
+        await contract.removeCustomer(1, { from: accounts[0] });
+        const count = await contract.count();
+        assert(count.toNumber() === 0, "Couldn't delete the customer.");
+    })
+
+
+
 });
